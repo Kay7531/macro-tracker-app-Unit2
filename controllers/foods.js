67343a626950e2c19a66e6ce1902d562fc9cc8cf
User Name <kauth333@gmail.com>
2023-01-15 22:43:51 -0500

    create css for view/foods/choice.ejs

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,9 +1,25 @@
 import { Food } from '../models/food.js'
 
 function index(req, res) {
-  console.log("food")
+    Food.find({})
+    .then(foods => {
+        res.render('foods/index', {
+            title: "All Foods",
+            foods: foods
+        })
+    })
+    .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
+function choiceFood(req, res) {
+  res.render("foods/choice", {
+    title: "Add Food",
+  })
 }
 
 export {
-  index
+  index,
+  choiceFood as choice,
 }
--- routes/foods.js
+++ routes/foods.js
@@ -4,6 +4,8 @@ import * as foodsCtrl from '../controllers/foods.js'
 const router = Router()
 
 router.get('/', foodsCtrl.index)
+// GET localhost:3000/foods/choice
+router.get('/choice', foodsCtrl.choice)
 
 export {
   router
