85a404e9a473e0c73b0f0ea9bd658c3a2237333c
User Name <kauth333@gmail.com>
2023-01-16 19:53:32 -0500

    add index router and controll function as well as show function and router

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,4 +1,18 @@
 import { Workout } from "../models/workout.js";
+function index(req, res) {
+    Workout.find({})
+    .then(workouts => {
+        res.render('workouts/index', {
+            title: "All Workouts",
+            workouts: workouts
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect("/")
+      })
+    }
+
 
 function newWorkout(req, res)  {
     res.render('workouts/new', {
@@ -17,7 +31,23 @@ function create(req, res) {
     })
 }
 
+function show(req, res) {
+    Workout.findById(req.params.id)
+    .then(workout => {
+       res.render('workouts/show', {
+        title: 'Workout Detail',
+        workout:workout
+       })
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect("/")
+      })
+    
+}
 export {
+    index,
     newWorkout as new,
     create,
+    show,
 }
--- routes/workouts.js
+++ routes/workouts.js
@@ -5,8 +5,12 @@ const router = Router()
 
 //GET localhost:3000/workouts/new
 router.get('/new', workoutsCtrl.new)
+//GET localhost:3000/workouts
+router.get('/', workoutsCtrl.index)
 // POST localhost:3000/workouts
 router.post('/', workoutsCtrl.create)
+// GET localhost:3000/workouts/:id
+router.get('/:id', workoutsCtrl.show)
 
 export {
   router
